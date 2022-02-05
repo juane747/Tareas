58f507ac29d3d48bbe77f71954d62ce7b2bf5658
Juane747 <emifive67@gmail.com>
2022-02-05 16:37:23 -0600

    primer cambio

---
+++ Actividad2.js
@@ -0,0 +1,17 @@
+var mes = 5;
+if(mes==12){
+    alert("es invierno")
+}else if(mes==1){
+    alert("es invierno")
+}else if(mes==2){
+    alert("es invierno")
+}else if(mes>=3 &&(mes <=5)){
+    alert("es primavera")
+}else if (mes>=6 &&(mes <=8)){
+    alert("es verano")
+}else if(mes>=9 && (mes <=11)){
+    alert("es otoño")
+}else{
+    alert("no es mes del año")
+}
+alert("Carnet: 0812201, Juan Emilio Garcia Perez")
